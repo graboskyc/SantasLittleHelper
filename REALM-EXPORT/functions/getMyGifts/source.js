0d6947794f5eb10ceaea5e99cff97d59b194b067
Chris Grabosky <chris@grabosky.net>
2020-11-25 10:58:40 -0500

    delete and unpurchase logic

--- REALM-EXPORT/functions/getMyGifts/source.js
+++ REALM-EXPORT/functions/getMyGifts/source.js
@@ -3,7 +3,7 @@ exports = async function(){
   var conn = context.services.get("mongodb-atlas").db("santa").collection("gifts");
   
   var pipeline = [
-    {$match:{ownerId:currentUser.id}},
+    {$match:{ownerId:currentUser.id, deleted:{$ne:true}}},
     {$sort: {_id:1}}
     ];
     
