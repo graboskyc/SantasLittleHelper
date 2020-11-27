987f469422f57b8c27794abeea733cae8a829803
Chris Grabosky <chris@grabosky.net>
2020-11-26 20:45:33 -0500

    basic logic for secret santa randomizer

--- SantasLittleHelper/Program.cs
+++ SantasLittleHelper/Program.cs
@@ -25,6 +25,8 @@ namespace SantasLittleHelper
 
     public static class Extensions
     {
+        
+    private static Random rng = new Random();  
         public static string FirstCharToUpper(this string str) {
             if (String.IsNullOrEmpty(str)) {
                 throw new ArgumentException("Input string is Empty!");
@@ -32,6 +34,18 @@ namespace SantasLittleHelper
 
             return char.ToUpper(str[0]) + str.Substring(1);
         }
+
+            public static void Shuffle<T>(this IList<T> list)  
+            {  
+                int n = list.Count;  
+                while (n > 1) {  
+                    n--;  
+                    int k = rng.Next(n + 1);  
+                    T value = list[k];  
+                    list[k] = list[n];  
+                    list[n] = value;  
+                }  
+            }
     }
 
 }
